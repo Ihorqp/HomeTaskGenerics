7d3f67ccd6585e7b3b4c6dcf7e8c3abff99ac6df
Zver <Zver@ZVERDVD-G0I5LTH>
2021-09-25 18:14:48 +0300

    Добавьте файлы проекта.

---
+++ HomeTaskGenerics/Program.cs
@@ -0,0 +1,74 @@
+﻿using HomeTaskGenerics.UserCollection;
+using System;
+using System.Text;
+
+namespace HomeTaskGenerics
+{
+    class Program
+    {
+		static void Main()
+		{
+			//var сollection = new UserCollection<Element>();
+
+			//сollection.Add(new Element(1, 2));
+			//сollection.Add(new Element(3, 4));
+			//сollection.Add(new Element(5, 6));
+			//сollection.Add(new Element(7, 8));
+
+			//foreach (var element in сollection)
+			//{
+			//	Console.WriteLine($",{element.FieldA} , {element.FieldB}");
+			//}
+
+			//Console.WriteLine(new string('-', 5));
+
+			//foreach (var element in сollection)
+			//{
+			//	Console.WriteLine($",{element.FieldA} , {element.FieldB}");
+			//}
+		//	Console.OutputEncoding = System.Text.Encoding.GetEncoding(1251);
+			
+			var сollection = new UserCollection<string>();
+
+			сollection.Add("some");
+			сollection.Add("tmp");
+			сollection.Add("text");
+			сollection.Add("for");
+
+			foreach (var element in сollection)
+			{
+				Console.WriteLine($",{element}");
+			}
+
+            Console.WriteLine(new string('-', 5));
+
+            foreach (var element in сollection)
+            {
+                Console.WriteLine($",{element}");
+            }
+            Console.WriteLine($"Кількість елементів у списку: {сollection.Count,6}");
+            Console.WriteLine($"Наявність значення text у списку: {сollection.Contains("text"),5}");
+            Console.WriteLine($"Видалення значення text iз списку: {сollection.Remove("text"),-1}");
+            Console.WriteLine($"Кількість елементів у списку: {сollection.Count,6}");
+            Console.WriteLine($"Наявність значення text у списку: {сollection.Contains("text"),5}");
+			Console.WriteLine($"Копіювання елементів у масив");
+
+			string[] arr = new string[сollection.Count];
+			сollection.CopyTo(arr, 0);
+
+			Console.WriteLine($"Видалення усіх елементів зi списку: {сollection.GetType()}");
+			сollection.Clear();
+
+			Console.WriteLine($"Кількість елементів у списку: {сollection.Count,6}");
+
+			Console.WriteLine($"відображення елементів з масива:");
+
+			foreach (var element in arr)
+			{
+				Console.WriteLine($",{element}");
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ HomeTaskGenerics/UserCollection/UserCollection.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HomeTaskGenerics.UserCollection
+{
+    #region Вузол однозв'язного списку
+    public class Node<T>
+    {
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public T Value 
+        {
+            get;
+            internal set;
+        }
+        public Node<T> Next
+        {
+            get;
+            internal set;
+        }
+
+        //public Node<T> Previous
+        //{
+        //    get;
+        //    internal set;
+        //}
+    }
+    #endregion
+
+    public class UserCollection<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        public Node<T> _head;
+        public Node<T> _tail;
+        public int Count
+        {
+            get;
+            private set;
+        }
+
+        #region Метод Add добавляє елемент в кінець зв`язного списку
+        public void Add(T value)
+        {
+            Node<T> node = new Node<T>(value);
+
+            if (_head == null)
+            {
+                _head = node;
+                _tail = node;
+            }
+
+            else
+            {
+                _tail.Next = node;
+                _tail = node;
+            }
+            Count++;
+        }
+        #endregion
+
+        #region Метод Remove видаляє із списку вказаний елемент.
+        public bool Remove(T item)
+        {
+            Node<T> previous = null;
+            Node<T> current = _head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item)) 
+                {
+
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+                        if (current.Next == null)
+                        {
+                            _tail = previous;
+                        }
+                    }
+                    else
+                    {
+                        _head = _head.Next;
+                        if (_head == null)
+                        {
+                            _tail = null;
+                        }
+                    }
+                    Count--;
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+        #endregion
+
+        #region Метод Clear очищає список
+
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            Count = 0;
+        }
+
+
+        #endregion
+
+        #region Метод Contains повертає значення true, якщо елемент належить списку
+        public bool Contains(T item)
+        {
+            Node<T> current = _head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+            return false;
+        }
+        #endregion
+
+        #region Метод CopyTo копіює вміст списку в массив
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Node<T> current = _head;
+
+            while (current != null)
+            {
+                array[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+        #endregion
+
+        T IEnumerator<T>.Current
+        {
+            get { return _tail.Value; }
+        }
+
+        public object Current => throw new NotImplementedException();
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = _head;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+
+        public bool MoveNext()
+        {
+            if (_tail.Next!=null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            _head=_tail;
+        }
+    }
+}
